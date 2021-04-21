96fc4e4bd8c804a1089d902a9aec0f9f78528522
gaye <gayegayemboup@gmail.com>
2021-04-21 15:07:08 +0200

    create a bill comptable

--- src/controllers/BillControllers.ts
+++ src/controllers/BillControllers.ts
@@ -1,8 +1,119 @@
 import { Request, Response } from 'express';
 import { Bill } from '../models/BillModel';
 import { updateBill } from '../helpers/checkFunction/editBill';
+import Datahelpers from '../helpers/Datahelpers';
+import { UserModel } from '../models/UsersModel';
+import mongoose from 'mongoose';
+import { Enterprise } from '../models/EntrepriseModel';
+import { BillComptable } from '../models/BillComptableModel';
+import { BillComptableInterface, BillServiceI } from '../interfaces/BillComptableInterface';
+import { Service } from '../models/ServiceModel';
 
 export class BillController {
+    static CreateBill = async (req: Request, res: Response) => {
+        try {
+            const { status, userId, entrepriseId, billNum, deadline, currency } = req.body;
+            if (!status || !userId || !entrepriseId || !billNum || !deadline) throw {code: 400};
+            if (!Datahelpers.billStatus(status)) throw {code: 401};
+            const userComptable: any = await UserModel.findOne({ _id: mongoose.Types.ObjectId(userId) });
+            if (!userComptable) throw {code: 402};
+            const enterprise: any = await Enterprise.findOne({_id: mongoose.Types.ObjectId(entrepriseId)});
+            if (!enterprise) throw {code : 403};
+            if (!await Datahelpers.CheckBillNumber(billNum)) throw {code: 404};
+            if (!Datahelpers.CheckDeadline(deadline)) throw {code: 405};
+            req.body.deadline = new Date(deadline);
+            req.body.articles = [];
+            req.body.totalHT = 0;
+            req.body.totalTTC = 0;
+
+            // Création de la facture
+            const billcomptable: any = await BillComptable.create(req.body);
+
+            // Envoi de la réponse
+            res.status(200).send({ error: false, message: 'Bill successfully created', bill: billcomptable });
+        } catch (err) {
+            if (err.code === 400)  res.status(400).send({ error: true, message: 'Une des données important sont manquants' });
+            else if (err.code === 401)  res.status(400).send({ error: true, message: 'Le status de la facture est invalide' });
+            else if (err.code === 401)  res.status(400).send({ error: true, message: 'Le status de la facture est invalide' });
+            else if (err.code === 402)  res.status(400).send({ error: true, message: 'Le comptable n\'existe pas' });
+            else if (err.code === 403)  res.status(400).send({ error: true, message: 'Verifier l\'Id de l\'entreprise' });
+            else if (err.code === 404)  res.status(400).send({ error: true, message: 'Le numéro de facture est invalide' });
+            else if (err.code === 405)  res.status(400).send({ error: true, message: 'Verifier la date de la facture' });
+            else Datahelpers.errorHandler(res, err);
+        }
+    }
+    static UpdateBillComptable = async (req: Request, res: Response) => {
+        try {
+            const { id, status, userId, billNum, currency, deadline } = req.body;
+            // Récupération de toutes les données du body
+            const services: Array<BillServiceI> = req.body.services;
+
+            // Vérification de si toutes les données nécessaire sont présentes
+            if (!id) throw new Error('Missing id field');
+
+            // Vérification de si la facture existe
+            const bill: any = await BillComptable.findOne({_id: mongoose.Types.ObjectId(id)});
+            if (!bill) throw new Error('Invalid bill id');
+
+            // Vérification de la validité du status
+            if (status && !Datahelpers.billStatus(status)) throw new Error('Invalid bill status');
+
+            // Vérification de si le client existe
+            if (userId) {
+                const comptable: any = await UserModel.findOne({_id: mongoose.Types.ObjectId(userId)});
+                if (!comptable) throw new Error('Invalid customer id');
+            }
+
+            // Vérification de la validité du numéro de facture
+            if (billNum && billNum !== bill.billNum && !await Datahelpers.CheckBillNumber(billNum)) throw new Error('Invalid bill number');
+
+            // Vérification de la validité de la date d'échéance
+            if (deadline && !Datahelpers.CheckDeadline(deadline)) throw new Error('Invalid deadline');
+
+            // Vérification de la validité des articles et mise à jour du prix HT et TTC
+            let STotalHT = 0;
+            let newTotalTTC = 0;
+            if (services) {
+                for (const service of services) {
+                    if (!service.serviceId || !service.duree) throw new Error('Invalid article format');
+                    const serviceFind: any = await Service.findOne({_id: mongoose.Types.ObjectId(service.serviceId)});
+                    if (!serviceFind) throw new Error('Some article id are invalid');
+                    STotalHT += (serviceFind.price * serviceFind.duree);
+                }
+            }
+
+            // Création des données existante à modifier
+            const updateBillC: any = {};
+
+            if (status) updateBillC.status = bill.status = status;
+            if (userId) updateBillC.userId = bill.userId = userId;
+            if (billNum) updateBillC.billNum = bill.billNum = billNum;
+            if (services) {
+                updateBillC.service = bill.service = services;
+                updateBillC.totalHT = STotalHT.toFixed(2);
+            }
+            if (currency) updateBillC.currency = bill.currency = currency;
+            if (deadline) updateBillC.deadline = bill.deadline = deadline;
+
+            // Modification de la facture
+            await updateBill(id, updateBillC);
+
+            // Récupération de la facture avec les articles
+            const billComptable: any = await BillComptable.findOne({_id: mongoose.Types.ObjectId(id)});
+
+            // Envoi de la réponse
+            res.status(200).send({ error: false, message: 'Bill successfully updated', bill: billComptable });
+        } catch (err) {
+            if (err.code === 400)  res.status(400).send({ error: true, message: 'Une des données important sont manquants' });
+            else if (err.code === 401)  res.status(400).send({ error: true, message: 'Le status de la facture est invalide' });
+            else if (err.code === 401)  res.status(400).send({ error: true, message: 'Le status de la facture est invalide' });
+            else if (err.code === 402)  res.status(400).send({ error: true, message: 'Le comptable n\'existe pas' });
+            else if (err.code === 403)  res.status(400).send({ error: true, message: 'Verifier l\'Id de l\'entreprise' });
+            else if (err.code === 404)  res.status(400).send({ error: true, message: 'Le numéro de facture est invalide' });
+            else if (err.code === 405)  res.status(400).send({ error: true, message: 'Verifier la date de la facture' });
+            else Datahelpers.errorHandler(res, err);
+        }
+    }
 
     static getBill = async (req: Request, res: Response) => {
         try {
