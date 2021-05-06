1bde614052f9b8f7cda236d642f15db4baa3c6de
gaye <gayegayemboup@gmail.com>
2021-05-06 22:50:44 +0200

    changement des dnnée de . env

--- src/middlewares/checkJwt.ts
+++ src/middlewares/checkJwt.ts
@@ -8,29 +8,33 @@ import { ComptableModel } from '../models/ComptableModel';
 
 config();
 
-const JWT_KEY: string = process.env.JWT_KEY ;
-export const getToken = async (authHeader: any) =>{
-    try{
-        return await authHeader.replace('Bearer ','').trim();   
-    }catch
+const JWT_KEY: string = process.env.JWT_KEY;
+export const getToken = async (authHeader: any) => {
+    try {
+        return await authHeader.replace('Bearer ', '').trim();
+    } catch
     {
         return false;
     }
 }
-export const getJwtPayload = async(token: string): Promise < any | null > => {
+export const getJwtPayload = async (token: string): Promise<any | null> => {
     try {
         const jwtObject = await jsonwebtoken.verify(token, JWT_KEY);
         if (jwtObject) {
             return jwtObject;
         }
-    } catch (err) {}
+    } catch (err) { }
     return null;
 };
 export const getAuthToken = async (user: any) => {
     const token = jsonwebtoken.sign({ _id: user._id, email: user.email }, JWT_KEY, { expiresIn: '24h' })
     user.token = token;
-    await ComptableModel.updateOne({ _id: mongoose.Types.ObjectId(user._id), email : user.email } ,{  $set: {token : user.token} });
+    await ComptableModel.updateOne({ _id: mongoose.Types.ObjectId(user._id), email: user.email }, { $set: { token: user.token } });
     return user;
 };
-
+export const getAuthRefreshToken = async (user: any) => {
+    user.refreshToken = jsonwebtoken.sign({ _id: user._id, email: user.email }, JWT_KEY, { expiresIn: '30d' });
+    await ComptableModel.updateOne({ _id: mongoose.Types.ObjectId(user._id) }, { refreshToken: user.refreshToken });
+    return user;
+};
 
